772ab144b2f14e11a8e0dac1348a95150eb8fba5
Pavel Nagorsky <pavelnagorsky@mail.ru>
2021-09-18 11:05:40 +0000

    v1

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 module.exports = function () {
 	var db = mongoose.connect(process.env.DBURL, { useNewUrlParser: true, 
-												   useUnifiedTopology: true,
-												   useFindAndModify: false
+												   useUnifiedTopology: true
 												 });
+	mongoose.set('useFindAndModify', false);
 	require('../models/user');
 	require('../models/post');
 	require('../models/comment');
